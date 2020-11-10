1275e9352e949f5801d33d5b6c4f6d363b07f162
Sagi23 <twigsagi@gmail.com>
2020-11-10 22:13:26 +0200

    Update gsap animation

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,7 @@ window.onscroll = function () {
     document.querySelector(".scroll-icon").style.opacity = "0%";
   }
 };
+
+gsap.from(".right", { duration: 0.7, opacity: 0, y: 100 });
+gsap.from(".left-up", { duration: 0.7, y: -200, opacity: 0, stagger: 0.9 });
+gsap.from(".left-down", { duration: 1, y: -100, delay: 0.8, opacity: 0 });
