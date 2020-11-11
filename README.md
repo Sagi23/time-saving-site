bf94f3174f75cfd1beee4142dfab4dd457f22214
Sagi23 <twigsagi@gmail.com>
2020-11-11 03:13:29 +0200

    Update site
    add animation and updated README

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ### Description
 
 My first attempt at creating a service selling site.  
+My first time using gsap library.  
 The site is responsive to all devices.
 
 ### The Stack i used:
@@ -12,11 +13,30 @@ html, scss/css, js, adobe xd.
 Made a mockup using adobe xd.  
 Created the mockup using html and scss.  
 I had some difficulties with making the site fully responsive but with the help of css grid and the beauty of scss i got the job done.  
-integrated the js to the html file.
+Had some fun playing around with the gsap library.  
+Integrated the js to the html file.
 
 ### What i learned:
 
 Improved scss skills.  
 Improved css grid skills.  
 Improved html slills.  
-How important the idea of mobile first thinking.
+How important the idea of mobile first thinking.  
+Improved docs reading.  
+Improved library integration.
+
+### The site:
+
+https://time-saving.netlify.app/
+
+### The mockup:
+
+![mockup](https://user-images.githubusercontent.com/73761063/98740463-c4096400-23b3-11eb-84da-cafba6faf345.png)
+
+### Desktop version:
+
+![Hnet-image](https://user-images.githubusercontent.com/73761063/98741679-c1a80980-23b5-11eb-9eee-c0682ca037ea.gif)
+
+### Mobile version:
+
+![ezgif com-gif-maker](https://user-images.githubusercontent.com/73761063/98742133-8f4adc00-23b6-11eb-8e53-679de0cd2218.gif)
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 let prevScroll = window.pageYOffset;
 window.onscroll = function () {
   let currentScrollpos = window.pageYOffset;
@@ -6,6 +8,137 @@ window.onscroll = function () {
   }
 };
 
-gsap.from(".right", { duration: 0.7, opacity: 0, y: 100 });
-gsap.from(".left-up", { duration: 0.7, y: -200, opacity: 0, stagger: 0.9 });
-gsap.from(".left-down", { duration: 1, y: -100, delay: 0.8, opacity: 0 });
+gsap.from(".right", { duration: 1, y: -300, opacity: 0 });
+gsap.from(".left-up", { duration: 1, y: -300, opacity: 0 });
+gsap.from(".left-down", { duration: 1, y: -100, opacity: 0, delay: 1 });
+
+gsap.set(".card", { y: 100 });
+
+ScrollTrigger.batch(".card", {
+  interval: 0.1,
+  batchMax: 3,
+  onEnter: (batch) =>
+    gsap.to(batch, {
+      opacity: 1,
+      y: 0,
+      stagger: { each: 0.15, grid: [1, 3] },
+    }),
+  onLeave: (batch) => gsap.set(batch, { opacity: 0, y: -100 }),
+  onEnterBack: (batch) => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15 }),
+  onLeaveBack: (batch) => gsap.set(batch, { opacity: 0, y: 100 }),
+  start: "20px bottom",
+  end: "top top",
+});
+
+ScrollTrigger.addEventListener("refreshInit", () =>
+  gsap.set(".card", { y: 0 })
+);
+gsap.set(".about-left", { y: 100 });
+
+ScrollTrigger.batch(".about-left", {
+  interval: 0.1,
+  batchMax: 3,
+  onEnter: (batch) =>
+    gsap.to(batch, {
+      opacity: 1,
+      y: 0,
+      stagger: { each: 0.15, grid: [1, 3] },
+      overwrite: true,
+    }),
+  onLeave: (batch) => gsap.set(batch, { opacity: 0, y: -100, overwrite: true }),
+  onEnterBack: (batch) =>
+    gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15, overwrite: true }),
+  onLeaveBack: (batch) =>
+    gsap.set(batch, { opacity: 0, y: 100, overwrite: true }),
+});
+
+ScrollTrigger.addEventListener("refreshInit", () =>
+  gsap.set(".about-left", { y: 0 })
+);
+gsap.set(".about-right", { y: 100 });
+
+ScrollTrigger.batch(".about-right", {
+  interval: 0.1,
+  batchMax: 3,
+  onEnter: (batch) =>
+    gsap.to(batch, {
+      opacity: 1,
+      y: 0,
+      stagger: { each: 0.15, grid: [1, 3] },
+      overwrite: true,
+    }),
+  onLeave: (batch) => gsap.set(batch, { opacity: 0, y: -100, overwrite: true }),
+  onEnterBack: (batch) =>
+    gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15, overwrite: true }),
+  onLeaveBack: (batch) =>
+    gsap.set(batch, { opacity: 0, y: 100, overwrite: true }),
+});
+
+ScrollTrigger.addEventListener("refreshInit", () =>
+  gsap.set(".about-right", { y: 0 })
+);
+
+gsap.set(".how-we-work-right", { y: 100 });
+
+ScrollTrigger.batch(".how-we-work-right", {
+  interval: 0.1,
+  batchMax: 3,
+  onEnter: (batch) =>
+    gsap.to(batch, {
+      opacity: 1,
+      y: 0,
+      stagger: { each: 0.15, grid: [1, 3] },
+      overwrite: true,
+    }),
+  onLeave: (batch) => gsap.set(batch, { opacity: 0, y: -100, overwrite: true }),
+  onEnterBack: (batch) =>
+    gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15, overwrite: true }),
+  onLeaveBack: (batch) =>
+    gsap.set(batch, { opacity: 0, y: 100, overwrite: true }),
+});
+
+ScrollTrigger.addEventListener("refreshInit", () =>
+  gsap.set(".how-we-work-right", { y: 0 })
+);
+gsap.set(".how-we-work-left", { y: 100 });
+
+ScrollTrigger.batch(".how-we-work-left", {
+  interval: 0.1,
+  batchMax: 3,
+  onEnter: (batch) =>
+    gsap.to(batch, {
+      opacity: 1,
+      y: 0,
+      stagger: { each: 0.15, grid: [1, 3] },
+      overwrite: true,
+    }),
+  onLeave: (batch) => gsap.set(batch, { opacity: 0, y: -100, overwrite: true }),
+  onEnterBack: (batch) =>
+    gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15, overwrite: true }),
+  onLeaveBack: (batch) =>
+    gsap.set(batch, { opacity: 0, y: 100, overwrite: true }),
+});
+
+ScrollTrigger.addEventListener("refreshInit", () =>
+  gsap.set(".how-we-work-left", { y: 0 })
+);
+gsap.set(".top", { y: 100 });
+
+ScrollTrigger.batch(".top", {
+  interval: 0.1,
+  batchMax: 3,
+  onEnter: (batch) =>
+    gsap.to(batch, {
+      opacity: 1,
+      y: 0,
+      stagger: { each: 0.15, grid: [1, 3] },
+      overwrite: true,
+    }),
+  onLeave: (batch) => gsap.set(batch, { opacity: 0, y: -100, overwrite: true }),
+  onEnterBack: (batch) =>
+    gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15, overwrite: true }),
+  onLeaveBack: (batch) =>
+    gsap.set(batch, { opacity: 0, y: 100, overwrite: true }),
+});
+
+ScrollTrigger.addEventListener("refreshInit", () => gsap.set(".top", { y: 0 }));
